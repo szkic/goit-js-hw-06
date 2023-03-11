5ce6b8a082a237d35c1fa1f4975463521cbc983f
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-11 18:47:10 +0100

    Task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.getElementById("ingredients");
+
+const ingredientsEls = ingredients.map((ingredient) => {
+  const listEls = document.createElement("li");
+  listEls.textContent = ingredient;
+  listEls.classList.add("item");
+  list.append(listEls);
+});
