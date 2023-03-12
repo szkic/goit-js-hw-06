0547e2b59eb9f38c82c897b558dee891fa1d6c7c
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-12 17:05:36 +0100

    Task 7

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const textEl = document.getElementById("text");
+const inputEl = document.getElementById("font-size-control");
+
+inputEl.addEventListener("input", (e) => {
+  textEl.style.fontSize = `${e.currentTarget.value}px`;
+});
