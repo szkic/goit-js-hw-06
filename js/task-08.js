94948f86968c0e21a4c2d73746b76198f7f3e182
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-13 17:11:27 +0100

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+const submitForm = (e) => {
+  e.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Proszę uzupełnić wsystkie pola");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  e.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", submitForm);
