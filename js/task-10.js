d29dbd4e49ecbcdbbf199dd17042b0377336e7b8
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-15 17:53:02 +0100

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
+const boxEls = document.getElementById("boxes");
+const controlsEl = document.getElementById("controls");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+const inputEl = controlsEl.firstElementChild;
+let dimensions = 20;
+
+// CHANGE COLOR
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// CREATE ELEMENTS
+const createBoxes = (amount) => {
+  amount = +inputEl.value;
+
+  for (let i = 0; i < amount; i++) {
+    const createBox = document.createElement("div");
+
+    createBox.setAttribute(
+      "style",
+      `background-color: ${getRandomHexColor()}; width: ${(dimensions += 10)}px; height: ${dimensions}px; margin-top: 20px`
+    );
+
+    boxEls.append(createBox);
+  }
+};
+createBtn.addEventListener("click", createBoxes);
+
+// DELETE ELEMENTS
+const destroyBoxes = () => (boxEls.innerHTML = "");
+destroyBtn.addEventListener("click", () => destroyBoxes());
