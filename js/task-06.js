27898bc3762ebc9c21aeb268b211b2e283a1d3e8
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-16 22:11:53 +0100

    refactor task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,7 @@
 const inputEl = document.getElementById("validation-input");
-const defLength = document.querySelector('input[data-length="6"]');
 
-inputEl.addEventListener("blur", (e) => {
-  const inputLength = e.currentTarget.value.length;
-
-  if (inputLength == defLength.dataset.length) {
-    inputEl.setAttribute("class", "valid");
-  } else {
-    inputEl.setAttribute("class", "invalid");
-  }
+inputEl.addEventListener("blur", () => {
+  inputEl.value.length == inputEl.dataset.length
+    ? inputEl.setAttribute("class", "valid")
+    : inputEl.setAttribute("class", "invalid");
 });
